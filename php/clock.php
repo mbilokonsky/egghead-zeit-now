b8877bbd038ad89d25997ee5e4b8871e87f67a8d
myk bilokonsky <mbilokonsky@gmail.com>
2016-11-06 19:22:39 +0100

    lesson 2: php in docker

---
+++ php/clock.php
@@ -0,0 +1,10 @@
+<html>
+  <head>
+    <title>Universe Clock</title>
+  </head>
+  <body>
+    <h1>Hello, Universe!</h1>
+    <h2>
+      <?php echo 'Current Time: '. date('m-d-Y H:m:s'); ?></h2>
+  </body>
+</html>
